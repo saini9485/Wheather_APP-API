43e1f9d3ad32ca432642d674700259c71a8271ca
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-28 16:52:32 +0530

    Wheather

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 //query selectors
-var cityInput = document.querySelector('.cityName');
-var btn1 = document.querySelector('.submitButton');
+var cityInput = document.querySelector(".cityName");
+var btn1 = document.querySelector(".submitButton");
+let container = document.querySelector(".container");
 //event Listeners
-btn1.addEventListener('click',function(name){
-    
+btn1.addEventListener("click", function (name) {
+container.style.display ="none"
+container.style.display ="block"
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=5142a079c21f6cb2df156c3a42bfec63`)//fetching api data
-    .then(response=>response.json())//text --> json
-    .then(data=>{
-        let result= `<div><h1>City Name : ${data.name}</h1> 
-            <h1>Temp = ${Math.floor(data["main"].temp-273)} °C</h1>
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=5142a079c21f6cb2df156c3a42bfec63`) //fetching api data
+    .then((response) => response.json()) //text --> json  
+    .then((data) => {
+      let result = `<div><h1>City Name : ${data.name}</h1> 
+            <h1>Temp = ${Math.floor(data["main"].temp - 273)} °C</h1>
             <h1> Pressure = ${data["main"].pressure} </h1>
             <h1> Humidity = ${data["main"].humidity} </h1></div>
-            <h1> country = ${data["sys"].country} </h1></div>`
-        let container = document.querySelector(".container")
-        container.innerHTML = result
-        console.log(data)
-    }).catch(err=>alert("invalid city name"))
-
-})
+            <h1> country = ${data["sys"].country} </h1></div>`;
+      container.innerHTML = result;
+      //console.log(data);
+    })
+    .catch((err) => alert("invalid city name")); 
+});
