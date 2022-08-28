a56349a5c0abcb2ff4b27bc4d4b6eadcc6b51621
Rajesh Saini <81741413+saini9485@users.noreply.github.com>
2022-08-28 11:24:20 +0530

    Wheather

---
+++ app.js
@@ -0,0 +1,19 @@
+//query selectors
+var cityInput = document.querySelector('.cityName');
+var btn1 = document.querySelector('.submitButton');
+//event Listeners
+btn1.addEventListener('click',function(name){
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=5142a079c21f6cb2df156c3a42bfec63`)//fetching api data
+    .then(response=>response.json())//text --> json
+    .then(data=>{
+        let result= `<div><h1>city : ${data.name}</h1> 
+            <h1>temp = ${Math.floor(data["main"].temp-273)} °C</h1>
+            <h1> pressure = ${data["main"].pressure} </h1>
+            <h1> humidity = ${data["main"].humidity} </h1></div>`
+        let container = document.querySelector(".container")
+        container.innerHTML = result
+        console.log(data)
+    }).catch(err=>alert("invalid city name"))
+
+})
